eb4067af8696cc03260e2c715f70adb24320d549
Payton Smith <smithpayton37@gmail.com>
2020-11-15 20:59:59 -0600

    Fundamentals Exercise 4 and Exercise 5

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -1,5 +1,7 @@
 package labs_examples.fundamentals.labs;
 
+import java.sql.SQLOutput;
+
 /**
  * Fundamentals Exercise 4: My Second Program
  *
@@ -16,7 +18,30 @@ public class Exercise_04 {
         System.out.println("int i is: " + i);
 
         // write your code below
-        byte b = 10;
+
+        byte by = 3;
+        System.out.println("byte b is: " + by);
+
+        short s = 7;
+        System.out.println("short s is: " + s);
+
+        int i2 = 37;
+        System.out.println("int i2 is: " + i2);
+
+        long l = 373l;
+        System.out.println("long l is: " + l);
+
+        float f = 373.73f;
+        System.out.println("float f is: " + f);
+
+        double d = 373.737373;
+        System.out.println("double d is: " + d);
+
+        boolean bo = true;
+        System.out.println("boolean bo is: " + bo);
+
+        char c = 'P';
+        System.out.println("char c is: " + c);
     }
 
 }
--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -1,6 +1,8 @@
 package labs_examples.fundamentals.labs;
 
 
+import java.sql.SQLOutput;
+
 /**
  * Fundamentals Exercise 5: Working with Strings
  *
@@ -14,14 +16,26 @@ public class Exercise_05 {
 
         String str = "hello!";
         // please declare an int variable below, and set it to the value of the length of "str"
+        int i = str.length();
 
         String str2 = "hello";
         // please initialize a boolean variable and test whether str is equal to str2
+        boolean equalStrings = str.equalsIgnoreCase(str2);
+        System.out.println("Do str and str2 match? " + equalStrings);
 
         // please concatenate str & str2 and set the result to a new String variable below
+        //???
+        System.out.println("The length of the String is " + str.length());
+        System.out.println("The length of the other String is " + str2.length());
+        // I don't know how to set the result to a new String variable
 
         // please demonstrate the use of any other method that is available to us in the String class
         // for example, replace(), substring(), contains(), indexOf() etc
+        System.out.println("All uppercase: " + str.toUpperCase());
+        System.out.println("All lowercase: " + str.toLowerCase());
+
+        System.out.println("Also all uppercase: " + str2.toUpperCase());
+        System.out.println("Also all lowercase: " + str2.toLowerCase());
 
     }
 
